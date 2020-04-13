cf1f2229866a40d749a9c0757aa4dc2d16c32191
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-13 16:07:12 +0300

    add import the formsmodule to display the form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +22,7 @@ import { RepositoryComponent } from './repository/repository.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [],
